631547751b14733b0e5a189a20bdd1b4e6edd2bf
MartaRudzite <marta.rudzite18@imperial.ac.uk>
2020-12-03 22:15:35 +0000

    lenght :p

--- day_3/ex_3_b.py
+++ day_3/ex_3_b.py
@@ -3,8 +3,8 @@ import numpy as np
 in_file = 'input_3.txt'
 lines = [line.rstrip('\n') for line in open(in_file)]
 
-lenght = len(lines[0])
-# print(lenght)
+length = len(lines[0])
+# print(length)
 
 right = [1,3,5,7,1]
 down = [1,1,1,1,2]
@@ -14,13 +14,13 @@ for idx, slope in enumerate(right):
     n_trees = 0
     i = 0
     for line in lines[::down[idx]]:
-        if line[i % lenght] == '#':
+        if line[i % length] == '#':
             n_trees += 1
         i += slope
     trees.append(n_trees)
 
 # print(np.prod(trees))
-# np.prod can't cope ;(
+# np.prod and math.prod can't cope ;(
 out = 1
 for i in trees:
     out *= i

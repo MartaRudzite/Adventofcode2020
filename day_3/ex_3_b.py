7cc88ca955887c2efbaf9e4f96d67c4faff0ad1d
MartaRudzite <marta.rudzite18@imperial.ac.uk>
2020-12-03 21:28:29 +0000

    sadness

---
+++ day_3/ex_3_b.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+in_file = 'input_3.txt'
+lines = [line.rstrip('\n') for line in open(in_file)]
+
+lenght = len(lines[0])
+# print(lenght)
+
+right = [1,3,5,7,1]
+down = [1,1,1,1,2]
+trees = []
+
+for idx, slope in enumerate(right):
+    n_trees = 0
+    i = 0
+    for line in lines[::down[idx]]:
+        if line[i % lenght] == '#':
+            n_trees += 1
+        i += slope
+    trees.append(n_trees)
+
+# print(np.prod(trees))
+# np.prod can't cope :(
+out = 1
+for i in trees:
+    out *= i
+print(out)
+# print(trees)

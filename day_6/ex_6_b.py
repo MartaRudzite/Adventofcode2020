f6c23e67ab2e22b9cb68805f2e96d113da885eed
MartaRudzite <marta.rudzite18@imperial.ac.uk>
2020-12-07 22:42:05 +0000

    part 2

---
+++ day_6/ex_6_b.py
@@ -0,0 +1,32 @@
+from collections import Counter
+
+# in_file = 'input_6_test.txt'
+in_file = 'input_6.txt'
+
+data = [line.rstrip() for line in open(in_file)]
+
+questions = []
+count = []
+n = 0
+
+for line in data:
+    if line != '':
+        q = list(line)
+        if n == 0:
+            [questions.append(i) for i in q]
+        else:
+            qq = []
+            for i in q:
+                if i in questions:
+                    qq.append(i)
+            questions = qq
+        n += 1
+    else:
+        count.append(len(questions))
+        # print(questions)
+        # count.append(count_unique(questions))
+        # print(count[-1])
+        questions = []
+        n = 0
+
+print(sum(count))

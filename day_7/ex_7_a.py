7681d48030fbe12e4fa47990b23c769d0190ed82
MartaRudzite <marta.rudzite18@imperial.ac.uk>
2020-12-09 22:58:43 +0000

    done for now, need to correct later

--- day_7/ex_7_a.py
+++ day_7/ex_7_a.py
@@ -3,8 +3,36 @@ in_file = 'input_7_test.txt'
 
 data = [line.rstrip() for line in open(in_file)]
 
+# contain = []
+up = {}
+
 for line in data:
     a, b = line.split('contain')
+    if 'no other bags' in b:
+        continue
     aa = a.split(' ')[0]+a.split(' ')[1]
-    print(aa)
-    print(b)
+    b = b.split(',')
+    for i in b:
+        # print(i.split(' '))
+        bb = i.split(' ')[2]+i.split(' ')[3]
+        if bb in up:
+            up[bb].append(aa)
+        else:
+            up[bb] = [aa]
+# print(up)
+
+queue = ['shinygold']
+outerbag = []
+
+while queue:
+    print(queue)
+    x = queue.pop(0)
+    if x in up:
+        for i in up[x]:
+            if (i not in queue) and (i not in outerbag):
+                queue.append(i)
+    else:
+        outerbag.append(x)
+
+print(outerbag)
+print(len(outerbag))

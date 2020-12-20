87b83113ea3ee96b23ef33c00848e343251abcc8
MartaRudzite <marta.rudzite18@imperial.ac.uk>
2020-12-20 22:23:37 +0000

    brute force solution for part 2 takes too long, will have to actually solve it

--- day_15/ex_15_a.py
+++ day_15/ex_15_a.py
@@ -3,15 +3,16 @@
 numbers = [1,20,13,6,12,0]         # puzzle input
 next = 17
 
+# end = 2020
+end = 30000000
 
-
-while len(numbers) <2020:
+while len(numbers) < end:
     try:
         i = numbers.index(next) +1
     except ValueError:
         i = 0
     numbers.insert(0,next)
     next = i
-    # print(numbers)
+    print(len(numbers))
 
 print(numbers[0])

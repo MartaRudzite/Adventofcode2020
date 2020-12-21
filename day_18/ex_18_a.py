7b3ac665caccec1d85894531042738fdfd928d80
MartaRudzite <marta.rudzite18@imperial.ac.uk>
2020-12-21 19:57:46 +0000

    part 1 solve for equation list

--- day_18/ex_18_a.py
+++ day_18/ex_18_a.py
@@ -1,4 +1,5 @@
 in_file = 'input_18.txt'
+# in_file = 'input_18_test.txt'
 equations = [line.rstrip('\n') for line in open(in_file)]
 # print(data)
 
@@ -9,46 +10,53 @@ sum = 0
 # test = '2 * 3 + (4 * 5)'
 # test = '5 + (8 * 3 + 9 + 3 * 4 * 3)'
 # test = '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))'
-test = '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2'
+# test = '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2'
 
-working_number = [0]
-working_action = ['+']
-pointer = 0
+def solve_equation(test):
+    working_number = [0]
+    working_action = ['+']
+    pointer = 0
 
-for i in test:
-    if i == ' ':
-        continue
-    elif i == '+' or i == '-' or i == '*':
-        working_action[pointer] = i
-    elif i == '(':
-        pointer +=1
-        working_number.append(0)
-        working_action.append('+')
-    elif i == ')':
-        pointer -=1
-        # print('')
-        # print(working_number)
-        x = working_number.pop()
-        # print(working_number)
-        # print('')
-        # print(working_action)
-        action = working_action.pop()
-        # print(working_action)
-        if working_action[pointer] == '+':
-            working_number[pointer] += x
-        elif working_action[pointer] == '-':
-            working_number[pointer] -= x
-        elif working_action[pointer] == '*':
-            working_number[pointer] *= x
-    else:
-        x = int(i)
-        if working_action[pointer] == '+':
-            working_number[pointer] += x
-        elif working_action[pointer] == '-':
-            working_number[pointer] -= x
-        elif working_action[pointer] == '*':
-            working_number[pointer] *= x
-        # print(i)
-        # print(working_number)
+    for i in test:
+        if i == ' ':
+            continue
+        elif i == '+' or i == '-' or i == '*':
+            working_action[pointer] = i
+        elif i == '(':
+            pointer +=1
+            working_number.append(0)
+            working_action.append('+')
+        elif i == ')':
+            pointer -=1
+            # print('')
+            # print(working_number)
+            x = working_number.pop()
+            # print(working_number)
+            # print('')
+            # print(working_action)
+            action = working_action.pop()
+            # print(working_action)
+            if working_action[pointer] == '+':
+                working_number[pointer] += x
+            elif working_action[pointer] == '-':
+                working_number[pointer] -= x
+            elif working_action[pointer] == '*':
+                working_number[pointer] *= x
+        else:
+            x = int(i)
+            if working_action[pointer] == '+':
+                working_number[pointer] += x
+            elif working_action[pointer] == '-':
+                working_number[pointer] -= x
+            elif working_action[pointer] == '*':
+                working_number[pointer] *= x
+            # print(i)
+            # print(working_number)
+    return working_number[0]
 
-print(working_number[0])
+sum = 0
+
+for equation in equations:
+    sum += solve_equation(equation)
+
+print(sum)
